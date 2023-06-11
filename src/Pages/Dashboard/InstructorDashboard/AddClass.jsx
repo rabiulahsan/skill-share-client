be5143e755b04ce0fb292e897e2bb57da0d93f01
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-06-12 03:55:27 +0600

    add a new class functional

--- src/Pages/Dashboard/InstructorDashboard/AddClass.jsx
+++ src/Pages/Dashboard/InstructorDashboard/AddClass.jsx
@@ -1,8 +1,11 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import useAuth from "../../../Hooks/useAuth/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AddClass = () => {
+  const [axiosSecure] = useAxiosSecure();
   const {
     register,
     handleSubmit,
@@ -12,9 +15,32 @@ const AddClass = () => {
 
   const { user } = useAuth();
 
-  console.log(user);
   const onSubmit = (data) => {
-    console.log(data);
+    const { image, class_name, ins_name, ins_email, available_seats, price } =
+      data;
+    const newClass = {
+      image,
+      class_name,
+      ins_name,
+      ins_email,
+      available_seats: parseInt(available_seats),
+      price: parseFloat(price),
+      status: "pending",
+      ratings: parseFloat((Math.random() * 1 + 4).toFixed(1)),
+    };
+    console.log(newClass);
+    axiosSecure.post("/addclass", newClass).then((data) => {
+      if (data.data.insertedId) {
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Class added successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
   return (
     <div className="px-[5%]">
@@ -28,19 +54,23 @@ const AddClass = () => {
             <input
               type="text"
               placeholder="Class Name"
-              {...register("name", { required: true, maxLength: 120 })}
+              {...register("class_name", { required: true, maxLength: 120 })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
+            {errors.class_name && (
+              <span className="text-red-600">Class Name is required</span>
+            )}
           </div>
           <div className="form-control w-full mb-4">
             <label className="label block text-gray-700 text-sm font-bold">
               <span className="label-text font-semibold">Instructor Name</span>
             </label>
             <input
+              readOnly
               type="text"
               placeholder="Instructor Name"
-              defaultValue={user?.displayName}
-              {...register("name", { required: true, maxLength: 120 })}
+              value={user?.displayName}
+              {...register("ins_name", { required: true, maxLength: 120 })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
           </div>
@@ -53,19 +83,23 @@ const AddClass = () => {
             <input
               type="text"
               placeholder="Display Image URL"
-              {...register("name", { required: true, maxLength: 120 })}
+              {...register("image", { required: true, maxLength: 120 })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
+            {errors.image && (
+              <span className="text-red-600">ImageURL is required</span>
+            )}
           </div>
           <div className="form-control w-full mb-4">
             <label className="label block text-gray-700 text-sm font-bold">
               <span className="label-text font-semibold">Instructor Email</span>
             </label>
             <input
-              type="text"
+              readOnly
+              type="email"
               placeholder="Instructor Email"
-              defaultValue={user?.email}
-              {...register("name", { required: true, maxLength: 120 })}
+              value={user?.email}
+              {...register("ins_email", { required: true, maxLength: 120 })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
           </div>
@@ -76,9 +110,12 @@ const AddClass = () => {
             <input
               type="number"
               placeholder="Available Seats"
-              {...register("name", { required: true, maxLength: 120 })}
+              {...register("available_seats", { required: true })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
+            {errors.available_seats && (
+              <span className="text-red-600">Available Seat is required</span>
+            )}
           </div>
           <div className="form-control w-full mb-4">
             <label className="label block text-gray-700 text-sm font-bold">
@@ -87,9 +124,12 @@ const AddClass = () => {
             <input
               type="number"
               placeholder="Price"
-              {...register("name", { required: true, maxLength: 120 })}
+              {...register("price", { required: true })}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight input input-bordered focus:outline-none focus:shadow-outline "
             />
+            {errors.price && (
+              <span className="text-red-600">Price is required</span>
+            )}
           </div>
         </div>
         <input
