3620f20103ad571e638f440169293cdbc2cd6955
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-06-11 13:48:05 +0600

    useVerifyAdmin hook created

---
+++ src/Hooks/useVerifyAdmin/useVerifyAdmin.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../useAuth/useAuth";
+import useAxiosSecure from "../useAxiosSecure/useAxiosSecure";
+
+const useVerifyAdmin = () => {
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+
+  //by tanstack query
+  const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+    queryKey: ["isAdmin", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+      return res.data.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
+};
+
+export default useVerifyAdmin;
